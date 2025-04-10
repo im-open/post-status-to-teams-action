bf6672f3acd31d6682529eacd75a8c7e6103ca8d
Chad Brill <cbchadb35@gmail.com>
2025-04-10 11:24:32 -0400

    Update to test out adaptive card for teams messaging

--- dist/index.js
+++ dist/index.js
@@ -31864,11 +31864,14 @@ var require_getTeamsNotificationBody = __commonJS({
         if (section.potentialAction && section.potentialAction.length > 0) {
           section.potentialAction.forEach(action => {
             adaptiveCardBody.body.push({
-              type: 'TextBlock',
-              text: `[${action.name}](${action.target[0]})`, // Hyperlink format
-              color: 'accent', // Makes the text blue
-              weight: 'Bolder', // Optional: Makes the text bold
-              wrap: true, // Ensures the text wraps if it's too long
+              type: 'ActionSet',
+              actions: [
+                {
+                  type: 'Action.OpenUrl',
+                  title: action.name,
+                  url: action.target[0]
+                }
+              ],
               spacing: 'Medium'
             });
           });
--- src/teams-notification/getTeamsNotificationBody.js
+++ src/teams-notification/getTeamsNotificationBody.js
@@ -55,11 +55,14 @@ var core2 = require_core();
         if (section.potentialAction && section.potentialAction.length > 0) {
           section.potentialAction.forEach(action => {
             adaptiveCardBody.body.push({
-              type: 'TextBlock',
-              text: `[${action.name}](${action.target[0]})`, // Hyperlink format
-              color: 'accent', // Makes the text blue
-              weight: 'Bolder', // Optional: Makes the text bold
-              wrap: true, // Ensures the text wraps if it's too long
+              type: 'ActionSet',
+              actions: [
+                {
+                  type: 'Action.OpenUrl',
+                  title: action.name,
+                  url: action.target[0]
+                }
+              ],
               spacing: 'Medium'
             });
           });
